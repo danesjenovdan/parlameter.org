5c52ce888d778c8a7d22115a000698119f374a1d
LepkoQQ <lepko.san@gmail.com>
2019-02-22 16:36:00 +0100

    more work

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
 import AOS from 'aos';
+import 'replaceme';
+
+// Polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
 
 AOS.init({
   offset: 120,
@@ -6,3 +12,18 @@ AOS.init({
   easing: 'ease',
   delay: 0,
 });
+
+const navLinks = document.querySelectorAll('.nav-link');
+if (navLinks) {
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+      });
+    });
+  });
+}
+
+// eslint-disable-next-line no-new
+new window.ReplaceMe(document.querySelector('.replace-me'));
