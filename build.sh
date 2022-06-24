9a742a543fb698f160364d1cd2cc155e23c557d5
muki <filipdobranic@gmail.com>
2022-06-24 14:11:43 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 sudo docker login rg.fr-par.scw.cloud/djnd -u nologin -p $SCW_SECRET_TOKEN
 
-sudo docker build -f parlameter/Dockerfile -t parlameter-landing:latest parlameter-landing
+sudo docker build -f parlameter/Dockerfile -t parlameter-landing:latest parlameter
 sudo docker tag parlameter-landing:latest rg.fr-par.scw.cloud/djnd/parlameter-landing:latest
 sudo docker push rg.fr-par.scw.cloud/djnd/parlameter-landing:latest
